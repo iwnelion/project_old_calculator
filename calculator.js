d979f7fe43b381e4d039a7f724b9192a7850a2c4
iwnelion <anname17@hotmail.com>
2024-07-02 23:24:11 +0300

    edit js calculations

--- calculator.js
+++ calculator.js
@@ -1,34 +1,73 @@
-// let n1=parseFloat(prompt("enter a number"));
-// let operator=prompt("enter an operator");
-// let n2=parseFloat(prompt("enter a number"));
+let operatorOption='';
+let nm1='';
+let nm2='';
 
-// function add(n1, n2){
-//     console.log(n1+n2);
-// }
-// function subtract(n1, n2){
-//     return n1-n2;
-// }
-// function multiply(n1, n2){
-//     return n1*n2;
-// }
-// function divide(n1, n2){
-//     if(n2!=0){
-//         return n1/n2;
-//     }else{
-//         return "can't divide by zero";
-//     }
-// }
-// function operate(op, num1, num2){
-//     if(operator=='+'){
-//         console.log(add(num1, num2));
-//     }else if(operator=='-'){
-//         console.log(subtract(num1, num2));
-//     }else if(operator=='*'){
-//         console.log(multiply(num1, num2));
-//     }else if(operator=='/'){
-//         console.log(divide(num1, num2));
-//     }else{
-//         console.log("wrong operator");
-//     }
-// }
-// operate(operator, n1, n2);
+function add(n1, n2){
+    return (n1+n2);
+}
+function subtract(n1, n2){
+    return n1-n2;
+}
+function multiply(n1, n2){
+    return n1*n2;
+}
+function divide(n1, n2){
+    if(n2!==0){
+        return n1/n2;
+    }else{
+        return "can't divide by zero";
+    }
+}
+
+function handleButton(value){
+    const display=document.getElementById('display');
+    if(isOperator(value)){
+        operatorOption=value;
+        display.textContent+=value;
+    }else if(value==='='){
+        const result=calc(parseFloat(nm1), operatorOption, parseFloat(nm2));
+        display.textContent=result;
+        nm1='';
+        operatorOption='';
+        nm2='';
+    }else{
+        if(!operatorOption){
+            nm1+=value;
+        }else{
+            nm2+=value;
+        }
+        display.textContent+=value;
+    }
+}
+
+function isOperator(value){
+    return ['+','-','*','/'].includes(value);
+}
+
+function calc(nm1, operatorOption, nm2){
+    switch(operatorOption){
+        case '+':
+            return add(nm1, nm2);
+        case '-':
+            return subtract(nm1, nm2);
+        case '*':
+            return multiply(nm1, nm2);
+        case '/':
+            return divide(nm1, nm2);
+        default:
+            return 'wrong operator';
+    }
+}
+
+const buttons=document.querySelectorAll('.num, .oper');
+for(let i=0;i<buttons.length;i++){
+    const button=buttons[i];
+    button.addEventListener('click', ()=>{
+        handleButton(button.textContent);
+    });
+}
+const clear=document.getElementById('clr');
+clear.addEventListener('click', ()=>{
+    const display=document.getElementById('display');
+    display.value='';
+});
